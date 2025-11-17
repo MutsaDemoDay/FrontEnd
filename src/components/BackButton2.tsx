b100619f51693fdf58ebed07875efc9276a6ce95
YJ <hghk0046@gmail.com>
2025-11-17 11:43:38 +0900

    fix: 브랜치 외 코드 일부 추가

---
+++ src/components/BackButton2.tsx
@@ -0,0 +1,14 @@
+import backButton2 from '../../assets/BackButton2.png';
+import { useNavigate } from 'react-router-dom';
+    
+export const BackButton2 = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <img src={backButton2} className='w-[48px] h-[48px]' alt="뒤로 가기" onClick={handleGoBack} />
+  );
+};
--- src/pages/Store/StoreInfo.tsx
+++ src/pages/Store/StoreInfo.tsx
@@ -1,7 +1,9 @@
+import { BackButton2 } from "../../components/BackButton2";
+
 export const StoreInfo = () => {
     return (
         <div className="flex flex-col w-screen h-[100vh] items-center justify-center border-b-[0.8px] border-black">
-            <p className=""></p>
+           <BackButton2 />
         </div>
     )
 };
