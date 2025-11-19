639bad05f489bf632b21ff670ae7918341e3ff69
YJ <hghk0046@gmail.com>
2025-11-19 17:53:35 +0900

    feat: 비밀번호 찾기 완성

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -83,6 +83,7 @@ export const FirstPage = () => {
       );
 
       if (!response.ok) {
+        alert('로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.');
         throw new Error(
           '로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.'
         );
